1f03d453e73a4ff374e9945cf613dc18c32310c7
최현준 <chj878194@naver.com>
2021-01-12 19:09:09 +0900

    Add Navigation.

--- src/App.js
+++ src/App.js
@@ -1,93 +1,32 @@
-import React from 'react';
-import { SafeAreaView, StyleSheet, ScrollView, View, Text, StatusBar, } from 'react-native';
-import { Header, LearnMoreLinks, Colors, DebugInstructions, ReloadInstructions, } from 'react-native/Libraries/NewAppScreen';
+import React, { Component } from 'react';
+import { SafeAreaView, View, Text} from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-const styles = StyleSheet.create({
-    scrollView: {
-        backgroundColor: Colors.lighter,
-    },
-    engine: {
-        position: 'absolute',
-        right: 0,
-    },
-    body: {
-        backgroundColor: Colors.white,
-    },
-    sectionContainer: {
-        marginTop: 32,
-        paddingHorizontal: 24,
-    },
-    sectionTitle: {
-        fontSize: 24,
-        fontWeight: '600',
-        color: Colors.black,
-    },
-    sectionDescription: {
-        marginTop: 8,
-        fontSize: 18,
-        fontWeight: '400',
-        color: Colors.dark,
-    },
-    highlight: {
-        fontWeight: '700',
-    },
-    footer: {
-        color: Colors.dark,
-        fontSize: 12,
-        fontWeight: '600',
-        padding: 4,
-        paddingRight: 12,
-        textAlign: 'right',
-    },
-});
+import BoardScreen from 'src/Board';
+import EditScreen from 'src/Edit';
+import LoginScreen from 'src/Login';
+import MainScreen from 'src/Main';
+import PostScreen from 'src/Post';
+import ProfileScreen from 'src/Profile';
 
+const StackNaigation = createStackNavigator();
 
-const App: () => React$Node = () => {
-    return (
-        <>
-            <StatusBar barStyle="dark-content" />
+class App extends Component {
+
+    render() {
+        return(  
             <SafeAreaView>
-                <ScrollView
-                    contentInsetAdjustmentBehavior="automatic"
-                    style={styles.scrollView}>
-                    <Header />
-                    {global.HermesInternal == null ? null : (
-                        <View style={styles.engine}>
-                            <Text style={styles.footer}>Engine: Hermes</Text>
-                        </View>
-                    )}
-                    <View style={styles.body}>
-                        <View style={styles.sectionContainer}>
-                            <Text style={styles.sectionTitle}>Step One</Text>
-                            <Text style={styles.sectionDescription}>
-                                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                                screen and then come back to see your edits.
-                            </Text>
-                        </View>
-                        <View style={styles.sectionContainer}>
-                            <Text style={styles.sectionTitle}>See Your Changes</Text>
-                            <Text style={styles.sectionDescription}>
-                                <ReloadInstructions />
-                            </Text>
-                        </View>
-                        <View style={styles.sectionContainer}>
-                            <Text style={styles.sectionTitle}>Debug</Text>
-                            <Text style={styles.sectionDescription}>
-                                <DebugInstructions />
-                            </Text>
-                        </View>
-                        <View style={styles.sectionContainer}>
-                            <Text style={styles.sectionTitle}>Learn More</Text>
-                            <Text style={styles.sectionDescription}>
-                                Read the docs to discover what to do next:
-                            </Text>
-                        </View>
-                        <LearnMoreLinks />
-                    </View>
-                </ScrollView>
+                <Stack.Navigator initialRouteName="Login">
+                    <Stack.Screen name="Board" component={BoardScreen} />
+                    <Stack.Screen name="Edit" component={EditScreen} />
+                    <Stack.Screen name="Login" component={LoginScreen} />
+                    <Stack.Screen name="Main" component={MainScreen} />
+                    <Stack.Screen name="Post" component={PostScreen} />
+                    <Stack.Screen name="Profile" component={ProfileScreen} />
+                </Stack.Navigator>
             </SafeAreaView>
-        </>
-    );
-};
+        );
+    }
+}
 
 export default App;
