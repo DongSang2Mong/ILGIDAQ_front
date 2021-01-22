65f42afd88951c2c808f1e0b6aedb4a32081078e
sshrik <chj878194@naver.com>
2021-01-22 16:25:10 +0900

    Kakao Login Finish

--- src/screen/Login/index.js
+++ src/screen/Login/index.js
@@ -3,33 +3,56 @@ import { Component } from 'react';
 import { View, Text, Image } from 'react-native';
 import KakaoLogins, { KAKAO_AUTH_TYPES } from '@react-native-seoul/kakao-login';
 
+class AppTitle extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <View>
+
+            </View>
+        );
+    }
+}
+
+
+
 export default class LoginScreen extends Component {
     constructor(props) {
         super(props);
     }
+
     componentDidMount() {
-        this.kakaoLogin();
+        // For Kakao Login, kakao Login -> Get profile
+        this.kakaoLogin()
+        .then((result) => {
+            // send 
+        });
     }
 
     kakaoLogin(){
-        KakaoLogins.login([KAKAO_AUTH_TYPES.Talk, KAKAO_AUTH_TYPES.Account])
-        .then(result => {
-            console.log(result);
-        })
-        .catch(err => {
-            console.log(err);
-            if (err.code === 'E_CANCELLED_OPERATION') {
-                console.log(`Login Cancelled:${err.message}`);
-                setLoginLoading(false);
-            } else {
-                console.log(err.code);
-            }
+        return new Promise((res, rej) => {
+            KakaoLogins.login([KAKAO_AUTH_TYPES.Talk, KAKAO_AUTH_TYPES.Account])
+            .then(result => {
+                KakaoLogins.getProfile()
+                .then(result => { res(result); })
+                .catch(err => { rej(err); });
+            })
+            .catch(err => { rej(err);  });
         });
     }
 
     render() {
         return (
             <View>
+                <View />
+                <View>
+                </View>
+                <View />
+                <View>
+                </View>
                 <View />
                 <View>
                 </View>
