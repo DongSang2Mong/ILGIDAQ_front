c3b0952d0513f7b0467d07da947317f117c6a9d8
sshrik <chj878194@naver.com>
2021-01-26 14:50:48 +0900

    Profile Component Define.

--- src/screen/Profile/index.js
+++ src/screen/Profile/index.js
@@ -1,10 +1,91 @@
 import React from 'react';
 import { Component } from 'react';
 import { View } from 'react-native';
+import { commonStyle, textStyle }from '../commonStyle';
 
+class Header extends Component {
+    constructor(props) {
+        super(props);
+    }
 
+    render() {
+        return(
+            <View>
 
+            </View>
+        );
+    }
+}
 
+class BottomTab extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return(
+            <View>
+
+            </View>
+        );
+    }
+}
+
+class UserName extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return(
+            <View>
+
+            </View>
+        );
+    }
+}
+
+class UserInfo extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return(
+            <View>
+
+            </View>
+        );
+    }
+}
+
+class PointInfo extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return(
+            <View>
+
+            </View>
+        );
+    }
+}
+
+class DiaryInfo extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return(
+            <View>
+
+            </View>
+        );
+    }
+}
 
 export default class ProfileScreen extends Component {
     constructor(props) {
@@ -14,7 +95,14 @@ export default class ProfileScreen extends Component {
     render() {
         return (
             <View>
-
+                <Header />
+                <View>
+                    <UserName />
+                    <UserInfo />
+                    <PointInfo />
+                    <DiaryInfo />
+                </View>
+                <BottomTab />
             </View>
         );
     }
