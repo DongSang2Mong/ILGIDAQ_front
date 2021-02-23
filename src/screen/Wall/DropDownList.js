19ee78dd0e6d8765a5819773ab3d2fd2afdb21c3
sshrik <chj878194@naver.com>
2021-02-23 13:48:53 +0900

    Drop Down List UI Change.

--- src/screen/Wall/DropDownList.js
+++ src/screen/Wall/DropDownList.js
@@ -7,6 +7,9 @@ import { showList } from "./exData"
 const UP = "up";
 const DOWN = "down"
 
+// TODO : Dropdown Btn Size를 props로 입력받아 top / left 를 변형해야한다.
+// TODO : Width size에 맞춰서 dropdown width를 변형해야한다.
+
 export default class DropDownList extends Component {
     constructor(props) {
         super(props);
@@ -34,27 +37,29 @@ export default class DropDownList extends Component {
         }
 
         var dropSpace = 
-            <View
-                style={{position: "absolute", top: 21, left: 124, backgroundColor: "#FFFFFF", zIndex: 20, borderBottomLeftRadius: 5, borderBottomRightRadius: 5}}
-            >
-                <FlatList 
-                    data={showList}
-                    renderItem={({ item }) => {
-                        return(<DropDownContents 
-                            textContents={item}
-                            onPress={this.changeListText}
-                        />);
-                    }}
-                    keyExtractor={(item, index) => index.toString()}
-                    style={{zIndex: 21, margin: 5}}
-                />
+            <View style={{position: "absolute", width: "60%", top: 21, backgroundColor: "#FFFFFF", zIndex: 20, borderBottomLeftRadius: 5, borderBottomRightRadius: 5}}>
+                <View
+                    style={{flex: 2}}
+                >
+                    <FlatList 
+                        data={showList}
+                        renderItem={({ item }) => {
+                            return(<DropDownContents 
+                                textContents={item}
+                                onPress={this.changeListText}
+                            />);
+                        }}
+                        keyExtractor={(item, index) => index.toString()}
+                        style={{zIndex: 21, margin: 8}}
+                    />
+                </View>
+                <View style={{flex: 1}} />
             </View>;
         if(this.state.rotate == DOWN) dropSpace = <View />;
         return (
             <View style={[this.props.style]}>
-                <View style={{flexDirection: "row"}}>
-                    <TouchableOpacity 
-                        style={{flexDirection: "row"}}
+                <View style={{flexDirection: "row", justifyContent: 'space-between'}}>
+                    <TouchableOpacity
                         onPress={() => {
                             var rotate = this.state.rotate == UP ? DOWN : UP;
                             this.setState({rotate: rotate});
@@ -62,9 +67,10 @@ export default class DropDownList extends Component {
                     >
                         {dropbtn}
                     </TouchableOpacity>
-                    <Text style={[textStyle.b20, {color: colorStyle.orange}]}>오늘</Text>
-                    <Text style={[textStyle.b20]}>의 일기</Text>
-                    <Text style={[textStyle.b18, { textAlignVertical: "bottom"}]}>({this.state.listText})</Text>
+                    <View style={{ flex: 1, alignItems: "center" }}>
+                        <Text style={[textStyle.b18, { textAlignVertical: "bottom" }]}>{this.state.listText}</Text>
+                    </View>
+                    <View style={{flex: 1}}></View>
                 </View>
                 {dropSpace}
             </View>
@@ -80,12 +86,12 @@ class DropDownContents extends Component {
     render() {
         return(
             <TouchableOpacity 
-                style={[this.props.style, {marginTop: 5, marginBottom: 5}]}
+                style={[this.props.style, {marginTop: 5, marginBottom: 5}, {alignItems: 'center'}]}
                 onPress={() => {
                     this.props.onPress(this.props.textContents);
                 }}
             >
-                <Text style={[textStyle.b12]}>{this.props.textContents}</Text>
+                <Text style={[textStyle.b16]}>{this.props.textContents}</Text>
             </TouchableOpacity>
         );
     }
